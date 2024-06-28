262e2eeadb341d5b1238b388c2b4ad6b53196202
riku-yanagihashi <riku.yanagihashi0420@gmail.com>
2024-06-29 01:37:52 +0900

    dksajdkljsa

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+extern crate rand;
+use rand::Rng;
+
+struct Answer {
+    answer: Vec<u8>,
+}
+
+impl Answer {
+    fn new(rng: &mut ThreadRng) -> Self {
+        let mut ans = vec![];
+        for _ in 0..4 {
+            ans.push(rng.gen_range(0..10));
+        }
+        return Self { answer: ans };
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut rng = rand::thread_rng();
+    let answer = Answer::new(&mut rng);
 }
